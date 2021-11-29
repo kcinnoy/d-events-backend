e00b8679cb7f4c46a9bafb108959fbffa0fc4bec
kcinnoy <yonnick00@gmail.com>
2021-11-29 15:35:12 +0000

    seperate backend repo

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '8d736317c860398d9f6aff48fc022bc9'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '155ae9e8-53e4-4f63-af64-c68d7d6b7632'
+};
